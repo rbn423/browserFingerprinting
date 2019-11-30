172655582b40d7556e09dd6522ca12321e3eb113
rbn423 <33626150+rbn423@users.noreply.github.com>
2019-11-30 19:07:19 +0100

    Insertado switch en una de las funciones de la base de datos

--- BD/BDCabecerasHTTP.php
+++ BD/BDCabecerasHTTP.php
@@ -2,19 +2,62 @@
 require_once(dirname(__DIR__)."/comun/config.php");
 
 class BDCabecerasHTTP {
-	
+
+	public static function selector($headers, $tipo){
+		if ($tipo == "select")
+			$separador = " AND ";
+		else
+			$separador = " , ";
+
+	}
+
 	public static function insertarCabecera($headers){
 		$app = Aplicacion::getSingleton();
 		$conn = $app->conexionBd();
-
+		$campo = NULL;
 		$i = 0;
 		$query = "INSERT INTO http (";
 		foreach($headers as $header => $value) {
-			if($header != "Cache-Control" && $header != "Host" && $header != "Cookie" && $header != "Referer") {
-				if ($i > 0 && $i < count($headers))
-					$query .= ", ";
-				$campo = str_replace("-", "", $header);
-				$query .= "$campo";
+			switch ($header) {
+				case "Accept":
+					$campo = "Accept";
+					break;
+				case "Accept-Language":
+					$campo = "AcceptLanguage";
+					break;
+				case "Upgrade-Insecure-Requests":
+					$campo = "UpgradeInsecureRequests";
+					break;
+				case "User-Agent":
+					$campo = "UserAgent";
+					break;
+				case "Accept-Encoding":
+					$campo = "AcceptEncoding";
+					break;
+				case "Connection":
+					$campo = "Connection";
+					break;
+				case "Sec-Fetch-Mode":
+					$campo = "SecFetchMode";
+					break;
+				case "Sec-Fetch-User":
+					$campo = "SecFetchUser";
+					break;
+				case "Sec-Fetch-Site":
+					$campo = "SecFetchSite";
+					break;
+				case "DNT":
+					$campo = "DNT";
+					break;
+				default :
+					$campo = NULL;
+					break;
+			}
+			if ($campo != NULL) {
+				if ($i == 0)
+					$query .= "$campo";
+				else
+					$query .= ", $campo";
 				$i++;
 			}
 		}
@@ -29,6 +72,7 @@ class BDCabecerasHTTP {
 			}
 		}
 		$query .= ")";
+		echo $query;
 		$conn->query($query);
 	}
 
@@ -60,7 +104,6 @@ class BDCabecerasHTTP {
 		$resultado = $conn->query($query);
 		$resultado = $resultado->fetch_all();
 		return $resultado[0][0];
-
 	}
 }
 
