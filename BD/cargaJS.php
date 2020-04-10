9777d64dbf79a9d232dd0b855a5bd49092af9445
rbn423 <33626150+rbn423@users.noreply.github.com>
2020-04-10 20:48:47 +0200

    Unicidad añadida
    Ahora se obtiene cuantas huellas como la del usuario existen en la base de datos. Para ello solo he modificado el archivo de cargaJS, que se ejecuta siempre de forma asíncrona cuando todo el código se ha ejecutado. Tras la última inserción en la tabla atributos se hacen tres select, de los cuales sacamos todos los atributos de las tablas a comprobar, que son las de atributos, audio y video. Las querys son específicas por dos motivos, el primero es que no nos interesa sacar ni el id ni la fecha de ninguna, ya que en todas las filas de cada tabla el id va a ser distinto y la fecha va a cambiar cada dia; la segunda es que es la única forma de que al hacer el fetch_assoc este salga bien, ya que el select * no devuelve los nombres de las columnas con esa función. Después de tener todos los datos hacemos un join de todas las tablas por el id y contamos todas las filas que tienen exactamente todos los atributos como los que hemos obtenido anteriormente mediante foreach que recorren los arrays asociativos y generan la consulta. Al final al resultado le restamos 1 porque se va a encontrar el propio id del que acaba de hacer la consulta.

--- BD/cargaJS.php
+++ BD/cargaJS.php
@@ -11,7 +11,7 @@ $query = "UPDATE `atributos` SET " ;
 echo "Elemento obtenidos mediante JS: ".count($_POST);//borrar mas adelante, de momento nos interesa saber si aparece algun elemento mas en el array
 foreach($_POST as $nombre_campo => $valor){
     if ($nombre_campo != "ID") {
-        $query .= "`" . $nombre_campo;
+        $query .= "` = " . $nombre_campo;
         if ($nombre_campo == "canvas"){
             $valor = md5($valor); //32 bytes
         }
@@ -27,35 +27,63 @@ foreach($_POST as $nombre_campo => $valor){
 $query .= " WHERE `ID` = '".$id."'";
 $conn->query($query);
 
-//obtenemos los elementos totales de http
-$query = "SELECT count(*) FROM `atributos`";
-$totales = $conn->query($query)->fetch_all()[0][0];
+//Estas querys hay que cambiarla en el caso de que añadamos algún campo a la tabla o lo eliminemos, ya que son muy especificas
+//se obtienen con el generador automatico de MySql y le eliminamos la fecha y el id que es lo unico que no queremos
+//QUERY DE ATRIBUTOS
+$query = "SELECT `Accept`, `AcceptLanguage`, `UpgradeInsecureRequests`, `UserAgent`, `AcceptEncoding`, `Connection`, 
+    `SecFetchMode`, `SecFetchUser`, `SecFetchSite`, `DNT`, `plataforma`, `userAgentJS`, `navegador`, `version`, 
+    `cookieEnabled`, `language`, `onLine`, `appName`, `zonaHoraria`, `screenWidth`, `screenHeight`, `screenAvailWidth`, 
+    `screenAvailHeight`, `screenColorDepth`, `screenPixelDepth`, `locationBar`, `pixelRatio`, `menuBar`, `personalBar`, 
+    `statusBar`, `toolBar`, `localStorage`, `sessionStorage`, `windowResults`, `indexDB`, `bateria`, `DNTJS`, 
+    `touchpoints`, `product`, `productSub`, `os`, `vendor`, `hardwareConcurrency`, `lenguajes`, `buildId`, `devMemory`, 
+    `flash`, `canvas`, `resumenFuentes`, `resumenPlugins` FROM `atributos` WHERE id = ". $id;
+$resultado = $conn->query($query);
+$resultadoAtributos = $resultado->fetch_assoc();
 
-//obtenemos el numero de elementos de la tabla que tienen los mismos valores JS
-$query = "SELECT count(*) FROM `atributos` WHERE ";
-$count = 1;
-foreach($_POST as $nombre_campo => $valor){
-    if ($nombre_campo != "ID") {
-        $query .= $nombre_campo;
-        if ($nombre_campo == "canvas"){
-            $canvas = md5($valor); //32 bytes
-            $query .= " = '".$canvas."'";
-        }
-        else {
-            if ($valor == "true" || $valor == "false")
-                $query .= " = " . $valor;
-            else
-                $query .= " = '" . $valor . "'";
-        }
-        if ($count < count($_POST) - 1)
-            $query .= " AND ";
-        $count++;
-    }
+//QUERY DE LOS FORMATOS DE AUDIO
+$query = "SELECT `ogg-vorbis`, `ogg-opus`, `3gpp`, `mp4-mp4a`, `mp4-mp3`, `mp4-ac3`, `mp4-ec3`, `acc`, `pcm`, 
+    `mpeg`, `flac`, `wave`, `webm-vorbis`, `mp3-mp3` FROM `formatosaudio` WHERE id = ".$id;
+$resultado = $conn->query($query);
+$resultadoAudio = $resultado->fetch_assoc();
+
+//QUERY DE LOS FORMATOS DE VIDEO
+$query = "SELECT `ogg-theora`, `ogg-vorbis`, `ogg-opus`, `mp4-avc1`, `mp4-mp4a`, `mp4-flac`, `webm-vp8`, `webm-vp9`,
+    `webm-vorbis` FROM `formatosvideo` WHERE id = ".$id;
+$resultado = $conn->query($query);
+$resultadoVideo = $resultado->fetch_assoc();
+
+//QUERY DE BUSQUE DE TODOS LOS ELEMENTOS QUE COINCIDAN CON LOS DEL NAVEGADOR ACTUAL
+$query = "SELECT count(*) FROM ((SELECT * FROM atributos)a JOIN (SELECT * FROM formatosaudio)b USING (id)) JOIN 
+    (SELECT * FROM formatosvideo)c USING (id) WHERE ";
+//ATRIBUTOS
+foreach ($resultadoAtributos as $nombre=>$valor){
+    if (is_null($valor))
+        $query .= "a.`".$nombre."` is null";
+    else
+        $query .= "a.`".$nombre."` = '".$valor."'";
+    $query .= " AND ";
 }
-$iguales = $conn->query($query)->fetch_all()[0][0];
+//AUDIO
+foreach ($resultadoAudio as $nombre=>$valor){
+    if (is_null($valor))
+        $query .= "b.`".$nombre."` is null";
+    else
+        $query .= "b.`".$nombre."` = '".$valor."'";
+    $query .= " AND ";
+}
+//VIDEO
+$count=0;
+foreach ($resultadoVideo as $nombre=>$valor){
+    if (is_null($valor))
+        $query .= "c.`".$nombre."` is null";
+    else
+        $query .= "c.`".$nombre."` = '".$valor."'";
+    if ($count < count($resultadoVideo) - 1)
+        $query .= " AND ";
+    $count++;
+}
 
-//mostramos la cantidad de casos iguales en la base de datos
-echo "<p>Existen ".($iguales-1)." entradas en nuestra base de datos con los mismos elementos obtenibles mediante javaScript.</p>";
-echo "<p>Los elementos obtenidos mediante javaScript de tu navegador son un <strong>".(100-(($iguales-1)*100/$totales))."% únicos</strong>.</p>";
-
+$resultado = $conn->query($query);
+$resultado = $resultado->fetch_all();
+echo "<p>Existen ".($resultado[0][0]-1)." browserFingerPrint como el tuyo en nuestra base de datos</p>";
 ?>
