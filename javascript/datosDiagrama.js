5019eb47633867c2529ef42a983bd9c7c92816a6
aagudo01 <aagudo01@ucm.es>
2020-04-22 20:53:21 +0200

    datosDiagrama estandarizando el dibujo

--- javascript/datosDiagrama.js
+++ javascript/datosDiagrama.js
@@ -1,9 +1,9 @@
-function cargaDiagrama() {
+function cargaDiagrama(elemento) {
+	var nombres = cargarNombres(elemento);
+	
 	// Load Charts and the corechart and barchart packages.
 	google.charts.load('current', {'packages':['corechart']});
-
-	// Draw the pie chart and bar chart when Charts is loaded.
-	google.charts.setOnLoadCallback(drawChart);
+	// Draw the pie chart and bar chart when Charts is loaded.	
 	
 	if (window.XMLHttpRequest) {
 		// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -13,35 +13,51 @@ function cargaDiagrama() {
 		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	
-	xmlhttp.open("GET","BD/cargaDiagrama.php",true);
+	if (elemento == "Navegador")
+		xmlhttp.open("GET","BD/cargaDiagramaNavegador.php",true);
+	else if (elemento == "Accept-Encoding")
+		xmlhttp.open("GET","BD/cargaDiagramaAcceptEncoding.php",true);
+
 	xmlhttp.responseType = 'json';
-	xmlhttp.send();	
+	xmlhttp.send();
+	
+	xmlhttp.onreadystatechange = function() {
+		if (this.readyState == 4 && this.status == 200) {
+		  google.charts.setOnLoadCallback(drawChart);
+		}
+	};	
 	
 	function drawChart() {
 		var data = new google.visualization.DataTable();
 		var num = xmlhttp.response;
 		data.addColumn('string', 'Topping');
 		data.addColumn('number', 'Slices');
-		data.addRows([		  
-		  ['Edge', num[0]],
-		  ['Chrome', num[1]],
-		  ['Firefox', num[2]],
-		  ['Opera', num[3]]
-		]);
+		for (var i=0; i<nombres.length; i++){
+			data.addRows([[nombres[i], num[i]]]);
+		}		
 		
 		//Piechart
-		var piechart_options = {title:'Pie Chart: Navegador',
+		var piechart_options = {title:'Pie Chart: ' + elemento,
 					   width:500,
 					   height:400};
 		var piechart = new google.visualization.PieChart(document.getElementById('piechart_div'));
-		piechart.draw(data, piechart_options);
+		piechart.draw(data, piechart_options);		
 		
 		//Barchart
-		var barchart_options = {title:'Barchart: Navegador',
+		/*var barchart_options = {title:'Barchart: Navegador',
 					   width:500,
 					   height:400,
 					   legend: 'none'};
 		var barchart = new google.visualization.BarChart(document.getElementById('barchart_div'));
-		barchart.draw(data, barchart_options);
+		barchart.draw(data, barchart_options);*/
 	}
+}
+
+function cargarNombres(elemento) {
+	var nombres;
+	if (elemento == "Navegador")
+		nombres = ['Edge','Chrome','Firefox','Opera'];
+	else if (elemento == "Accept-Encoding")
+		nombres = ['gzip, deflate, br','gzip, deflate'];
+	return nombres;
 }
