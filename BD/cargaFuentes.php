d4085b5f48aeea1420964f11d71524ababa473c1
Jesús Martín <20012803+esu23@users.noreply.github.com>
2020-05-06 19:24:11 +0200

    Consultas seguras en Fuentes y Plugins

--- BD/cargaFuentes.php
+++ BD/cargaFuentes.php
@@ -10,14 +10,22 @@ $conn = $app->conexionBd();
 //insertamos en la base de datos los tipos de fuentes
 foreach ($_POST as $nombre_fuente => $fuente){
     if ($nombre_fuente != "ID" && $nombre_fuente != "resumen") {
-        $query = "INSERT INTO `fuentes`(`id`, `nombreFuente`) VALUES (".$id.",'".$fuente."')";
-        $conn->query($query);
+        //$query = "INSERT INTO `fuentes`(`id`, `nombreFuente`) VALUES (".$id.",'".$fuente."')";
+        //$conn->query($query);
+        $stmt = $conn->prepare("INSERT INTO `fuentes`(`id`, `nombreFuente`) VALUES (?, ?)");
+        $stmt->bind_param("is", $id, $fuente);
+        $stmt->execute();
+        $stmt->close();
     }
 }
 
 //insertamos el resumen en la tabla atributos
-$query = "UPDATE `atributos` SET `resumenFuentes`= '".$resumen."' WHERE `ID` = '".$id."'";
-$conn->query($query);
+//$query = "UPDATE `atributos` SET `resumenFuentes`= '".$resumen."' WHERE `ID` = '".$id."'";
+//$conn->query($query);
+$stmt = $conn->prepare("UPDATE `atributos` SET `resumenFuentes`=? WHERE `ID` =?");
+$stmt->bind_param("si", $resumen, $id);
+$stmt->execute();
+$stmt->close();
 
 //Total de resumenes de fuentes distintos
 $query_total = "SELECT count(`resumenFuentes`) FROM `atributos`";
@@ -25,27 +33,43 @@ $total_reg = $conn->query($query_total);
 $total_reg = $total_reg->fetch_all();
 
 //Cogemos el resumenFuentes de quien ha hecho la petición
-$query = "SELECT `resumenFuentes` FROM `atributos` WHERE id = ".$id;
-$resultado = $conn->query($query);
+//$query = "SELECT `resumenFuentes` FROM `atributos` WHERE id = ".$id;
+//$resultado = $conn->query($query);
+//$resultado = $resultado->fetch_assoc();
+$stmt = $conn->prepare("SELECT `resumenFuentes` FROM `atributos` WHERE id =?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$resultado = $stmt->fetch_result();
 $resultado = $resultado->fetch_assoc();
+$stmt->close();
 
-//Lo usaremos para devolver el JSON
-$arrayRatio = array();
-
-//Vemos cuantos elementos hay con ese resumen
+//Vemos cuantos elementos hay
 foreach ($resultado as $valor){
-    $query_ratio = "SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes`";
-    if (is_null($valor))
-        $query_ratio .= "`".$valor."` is null";
-    else
-        $query_ratio .= "= '".$valor. "'";
+    //$query_ratio = "SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes`";
+    if (is_null($valor)) {
+        //$query_ratio .= "`".$valor."` is null";
+        $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =? is null");
+        $stmt->bind_param("s", $valor);
+    }
+    else {
+        //$query_ratio .= "= '".$valor. "'";
+        $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =?");
+        $stmt->bind_param("s", $valor);
+    }
 }
 
-$single_ratio = $conn->query($query_ratio);
+//$single_ratio = $conn->query($query_ratio);
+//$single_ratio = $single_ratio->fetch_all();
+$stmt->execute();
+$single_ratio = $stmt->fetch_result();
 $single_ratio = $single_ratio->fetch_all();
+$stmt->close();
 
 $ratio =  $single_ratio[0][0] * 100 / $total_reg[0][0];
 $ratio = round($ratio, 2);
+
+//Lo usaremos para devolver el JSON
+$arrayRatio = array();
 $arrayRatio += ["resumenFuentes" => $ratio."%"];
 echo json_encode($arrayRatio);
 ?>
--- BD/cargaPlugins.php
+++ BD/cargaPlugins.php
@@ -10,14 +10,23 @@ $conn = $app->conexionBd();
 //insertamos en la base de datos los plugins
 foreach ($_POST as $nombre_plugin => $plugin){
     if ($nombre_plugin != "ID" ) {
-        $query = "INSERT INTO `plugins`(`id`, `nombrePlugin`) VALUES (".$id.",'".$plugin."')";
-        $conn->query($query);
+        //$query = "INSERT INTO `plugins`(`id`, `nombrePlugin`) VALUES (".$id.",'".$plugin."')";
+        //$conn->query($query);
+        $stmt = $conn->prepare("INSERT INTO `plugins`(`id`, `nombrePlugin`) VALUES (?, ?)");
+        $stmt->bind_param("is", $id, $plugin);
+        $stmt->execute();
+        $stmt->close();
     }
 }
 
 //insertamos el resumen en la tabla atributos
-$query = "UPDATE `atributos` SET `resumenPlugins`= '".$resumen."' WHERE `ID` = '".$id."'";
-$conn->query($query);
+//$query = "UPDATE `atributos` SET `resumenPlugins`= '".$resumen."' WHERE `ID` = '".$id."'";
+//$conn->query($query);
+$stmt = $conn->prepare("UPDATE `atributos` SET `resumenPlugins`=? WHERE `ID` =?");
+$stmt->bind_param("si", $resumen, $id);
+$stmt->execute();
+$stmt->close();
+
 
 //Total de plugins distintos: Tal como guardamos los datos no se puede hacer de esta manera
 //$query_total = "SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins`";
@@ -25,37 +34,52 @@ $conn->query($query);
 //$total_reg = $total_reg->fetch_all();
 
 //Cogemos los plugins de quien ha hecho la petición
-$query = "SELECT `nombrePlugin` FROM `plugins` WHERE id = ".$id;
-$resultado = $conn->query($query);
+//$query = "SELECT `nombrePlugin` FROM `plugins` WHERE id = ".$id;
+//$resultado = $conn->query($query);
+//$resultado = $resultado->fetch_assoc();
+$stmt = $conn->prepare("SELECT `nombrePlugin` FROM `plugins` WHERE id =?");
+$stmt->bind_param("i", $id);
+$stmt->execute();
+$resultado = $stmt->fetch_result();
 $resultado = $resultado->fetch_assoc();
+$stmt->close();
 
 //Número total de plugins: Tal como guardamos los datos no se puede hacer de esta manera
 //$query_num_plugins = "SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins` WHERE id = ".$id;
 //$num_plugins = $conn->query($query_num_plugins);
 //$num_plugins = $num_plugins->fetch_all();
 
-//Lo usaremos para devolver el JSON
-$arrayRatio = array();
-
 //Vemos cuantos elementos hay
 foreach ($resultado as $valor){
-    $query_ratio = "SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins` WHERE `nombrePlugin`";
-    if (is_null($valor))
-        $query_ratio .= "`".$valor."` is null";
-    else
-        $query_ratio .= "= '".$valor. "'";
+    //$query_ratio = "SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins` WHERE `nombrePlugin`";
+    if (is_null($valor)) {
+        //$query_ratio .= "`".$valor."` is null";
+        $stmt = $conn->prepare("SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins` WHERE `nombrePlugin` =? is null");
+        $stmt->bind_param("s", $valor);
+    }
+    else {
+        //$query_ratio .= "= '".$valor. "'";
+        $stmt = $conn->prepare("SELECT COUNT(DISTINCT `nombrePlugin`) FROM `plugins` WHERE `nombrePlugin` =?");
+        $stmt->bind_param("s", $valor);
+    }
 }
 
-$single_ratio = $conn->query($query_ratio);
+//$single_ratio = $conn->query($query_ratio);
+//$single_ratio = $single_ratio->fetch_all();
+$stmt->execute();
+$single_ratio = $stmt->fetch_result();
 $single_ratio = $single_ratio->fetch_all();
+$stmt->close();
 
 $ratio =  $single_ratio[0][0] * 100 / $total_reg[0][0];
 $ratio = round($ratio, 2);
-$arrayRatio += ["resumenPlugins" => $ratio."%"]; //ratioPlugin
 
 //$ratio_num_plugs = $num_plugins[0][0] * 100 / $total_reg[0][0];
 //$ratio_num_plugs = round($ratio_num_plugs, 2);
-//$arrayRatio += ["ratio_total_plugins" => $ratio_num_plugs."%"];
 
+//Lo usaremos para devolver el JSON
+$arrayRatio = array();
+$arrayRatio += ["resumenPlugins" => $ratio."%"]; //ratioPlugin
+//$arrayRatio += ["ratio_total_plugins" => $ratio_num_plugs."%"];
 echo json_encode($arrayRatio);
 ?>
