efb4be74fa8966c4dfbaaf8a898f077308324d53
Jesús Martín <20012803+esu23@users.noreply.github.com>
2020-05-09 17:11:58 +0200

    CargaFuentes corregido.

--- BD/cargaFuentes.php
+++ BD/cargaFuentes.php
@@ -10,8 +10,6 @@ $conn = $app->conexionBd();
 //insertamos en la base de datos los tipos de fuentes
 foreach ($_POST as $nombre_fuente => $fuente){
     if ($nombre_fuente != "ID" && $nombre_fuente != "resumen") {
-        //$query = "INSERT INTO `fuentes`(`id`, `nombreFuente`) VALUES (".$id.",'".$fuente."')";
-        //$conn->query($query);
         $stmt = $conn->prepare("INSERT INTO `fuentes`(`id`, `nombreFuente`) VALUES (?, ?)");
         $stmt->bind_param("is", $id, $fuente);
         $stmt->execute();
@@ -20,8 +18,6 @@ foreach ($_POST as $nombre_fuente => $fuente){
 }
 
 //insertamos el resumen en la tabla atributos
-//$query = "UPDATE `atributos` SET `resumenFuentes`= '".$resumen."' WHERE `ID` = '".$id."'";
-//$conn->query($query);
 $stmt = $conn->prepare("UPDATE `atributos` SET `resumenFuentes`=? WHERE `ID` =?");
 $stmt->bind_param("si", $resumen, $id);
 $stmt->execute();
@@ -33,42 +29,39 @@ $total_reg = $conn->query($query_total);
 $total_reg = $total_reg->fetch_all();
 
 //Cogemos el resumenFuentes de quien ha hecho la petición
-//$query = "SELECT `resumenFuentes` FROM `atributos` WHERE id = ".$id;
-//$resultado = $conn->query($query);
-//$resultado = $resultado->fetch_assoc();
 $stmt = $conn->prepare("SELECT `resumenFuentes` FROM `atributos` WHERE id =?");
 $stmt->bind_param("i", $id);
 $stmt->execute();
-$resultado = $stmt->fetch_result();
-$resultado = $resultado->fetch_assoc();
+$stmt -> store_result();
+$stmt -> bind_result($resumenFuentes);
+$stmt -> fetch();
 $stmt->close();
 
-//Vemos cuantos elementos hay
-foreach ($resultado as $valor){
-    //$query_ratio = "SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes`";
-    if (is_null($valor)) {
-        //$query_ratio .= "`".$valor."` is null";
-        $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =? is null");
-        $stmt->bind_param("s", $valor);
-    }
-    else {
-        //$query_ratio .= "= '".$valor. "'";
-        $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =?");
-        $stmt->bind_param("s", $valor);
-    }
+//Contamos cuantas veces está dicho resumenFuentes en la BD
+if (is_null($resumenFuentes)) {
+    $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =? is null");
+    $stmt->bind_param("s", $resumenFuentes);
+    $stmt->execute();
+    $stmt -> store_result();
+    $stmt -> bind_result($count);
+    $stmt -> fetch();
+    $stmt->close();
+}
+else {
+    $stmt = $conn->prepare("SELECT count(`resumenFuentes`) FROM `atributos` WHERE `resumenFuentes` =?");
+    $stmt->bind_param("s", $resumenFuentes);
+    $stmt->execute();
+    $stmt -> store_result();
+    $stmt -> bind_result($count);
+    $stmt -> fetch();
+    $stmt->close();
 }
 
-//$single_ratio = $conn->query($query_ratio);
-//$single_ratio = $single_ratio->fetch_all();
-$stmt->execute();
-$single_ratio = $stmt->fetch_result();
-$single_ratio = $single_ratio->fetch_all();
-$stmt->close();
-
-$ratio =  $single_ratio[0][0] * 100 / $total_reg[0][0];
+//Calculamos el ratio
+$ratio =  $count * 100 / $total_reg[0][0];
 $ratio = round($ratio, 2);
 
-//Lo usaremos para devolver el JSON
+//Devolvemos el json
 $arrayRatio = array();
 $arrayRatio += ["resumenFuentes" => $ratio."%"];
 echo json_encode($arrayRatio);
