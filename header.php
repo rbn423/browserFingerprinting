eaa3744c43b4b57f6aceaa5e65c2c7583ed8f258
Jesús Martín <20012803+esu23@users.noreply.github.com>
2020-05-10 10:42:41 +0200

    Header.php con los includes

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+    <meta charset="utf-8">
+
+    <script type="text/javascript" src="javascript/navigator.js"></script>
+    <script type="text/javascript" src="javascript/screen.js"></script>
+    <script type="text/javascript" src="javascript/fecha.js"></script>
+    <script type="text/javascript" src="javascript/plugins.js"></script>
+    <script type="text/javascript" src="javascript/window.js"></script>
+    <script type="text/javascript" src="javascript/fuentes.js"></script>
+    <script type="text/javascript" src="javascript/fontdetect.js"></script>
+    <script type="text/javascript" src="javascript/asincrono.js"></script>
+    <script type="text/javascript" src="javascript/canvas.js"></script>
+    <script type="text/javascript" src="javascript/video.js"></script>
+    <script type="text/javascript" src="javascript/audio.js"></script>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
--- index.php
+++ index.php
@@ -8,28 +8,8 @@
 	$iguales = BDCabecerasHTTP::numeroCabecerasIguales($headers);
 	$totales = BDCabecerasHTTP::cabecerasTotales();
 
+    require "header.php";
 ?>
-
-<!DOCTYPE html>
-<html>
-	<head>
-        <title><?php echo $title; ?></title>
-        <meta charset="utf-8">
-
-		<script type="text/javascript" src="javascript/navigator.js"></script>
-		<script type="text/javascript" src="javascript/screen.js"></script>
-		<script type="text/javascript" src="javascript/fecha.js"></script>
-		<script type="text/javascript" src="javascript/plugins.js"></script>
-		<script type="text/javascript" src="javascript/window.js"></script>
-		<script type="text/javascript" src="javascript/fuentes.js"></script>
-		<script type="text/javascript" src="javascript/fontdetect.js"></script>
-		<script type="text/javascript" src="javascript/asincrono.js"></script>
-		<script type="text/javascript" src="javascript/canvas.js"></script>
-		<script type="text/javascript" src="javascript/video.js"></script>
-		<script type="text/javascript" src="javascript/audio.js"></script>
-		<link rel="stylesheet" href="css/estilos.css">
-	</head>
-	<body>
 		<div id="container">
 			<div id="linkDiagramas"><a href="diagramas.php">Diagramas</a></div>
             <div id="resultadoJS"></div>
